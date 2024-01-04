54943b945b5a93de64cc88e4350fdc9c664fd50d
zhangsansi <85163357+sansi-zhang@users.noreply.github.com>
2024-01-04 11:16:54 +0800

    Add files via upload

---
+++ Hardware/hardwarecode/L3FNet-Hardcode/HLSCode/Batch18/top_StreamingDataflowPartition_1_StreamingDataWidthConverter_Batch_18.cpp
@@ -0,0 +1,21 @@
+
+#define AP_INT_MAX_W 72
+
+#include "bnn-library.h"
+
+// includes for network parameters
+#include "streamtools.h"
+
+// defines for network parameters
+#define InWidth 36 
+#define OutWidth 72 
+#define NumInWords 46080 
+#define numReps 1
+
+void StreamingDataflowPartition_1_StreamingDataWidthConverter_Batch_18(hls::stream<ap_uint<36> > &in0, hls::stream<ap_uint<72> > &out)
+{
+#pragma HLS INTERFACE axis port=in0 name=in0_V
+#pragma HLS INTERFACE axis port=out name=out_V
+#pragma HLS INTERFACE ap_ctrl_none port=return
+StreamingDataWidthConverter_Batch<InWidth, OutWidth, NumInWords>(in0, out, numReps);
+}
